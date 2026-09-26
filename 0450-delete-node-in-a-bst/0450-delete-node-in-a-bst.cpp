ac51f2645699d0de81343c372410f36c80c26b06
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-26 19:13:20 +0530

    Time: 0 ms (100%), Space: 34.4 MB (14.66%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int minimum(TreeNode* root){
+        while(root->left != NULL){
+            root = root->left;
+        }
+
+        return root->val;
+    }
+
+    
+    TreeNode* DeleteFromBst(TreeNode* root, int val){
+        if(root == NULL){
+            return NULL;
+        }
+        else if(root->val == val){
+            // 0 node
+            if(root->left == NULL && root->right == NULL){
+                delete root;
+                return NULL;
+            }
+
+            //1 node
+
+            if(root->left != NULL && root->right == NULL){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            if(root->right != NULL && root->left == NULL){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            
+            // 2 node
+
+            if(root->right != NULL && root->left != NULL){
+             int mini = minimum(root->right);
+             root->val = mini;
+             root->right = DeleteFromBst(root->right,mini);
+             return root;
+            }
+        }
+        else if(root->val < val){
+            root->right = DeleteFromBst(root->right,val);
+            return root;
+           
+        }
+        else{
+            root->left = DeleteFromBst(root->left,val);
+            return root;
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+        root = DeleteFromBst(root,key);
+        return root;
+        
+    }
+};
